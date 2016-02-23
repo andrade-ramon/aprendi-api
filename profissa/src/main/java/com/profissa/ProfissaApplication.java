7addb38e0ead6649dee36a43988e5958a5357bda
Ramon Andrade <andrade.rmn@gmail.com>
2016-02-23 19:32:53 -0300

    gradle setup

---
+++ profissa/src/main/java/com/profissa/ProfissaApplication.java
@@ -0,0 +1,12 @@
+package com.profissa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfissaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfissaApplication.class, args);
+	}
+}
