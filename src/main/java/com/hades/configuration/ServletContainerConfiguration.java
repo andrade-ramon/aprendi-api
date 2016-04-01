1cfa3b966215d768ed218903eef120baf4b423a5
Eric Dallo <ericdallo06@hotmail.com>
2016-03-31 22:02:46 -0300

    Altera servlet para undertow e adiciona http2

--- src/main/java/com/hades/configuration/ServletContainerConfiguration.java
+++ src/main/java/com/hades/configuration/ServletContainerConfiguration.java
@@ -1,14 +1,26 @@
 package com.hades.configuration;
 
+import static io.undertow.UndertowOptions.ENABLE_HTTP2;
+
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
 class ServletContainerConfiguration implements EmbeddedServletContainerCustomizer {
 
 	@Override
 	public void customize(ConfigurableEmbeddedServletContainer container) {
 		container.setPort(6660);
+	}
+	
+	@Bean
+	UndertowEmbeddedServletContainerFactory embeddedServletContainerFactory() {
+		UndertowEmbeddedServletContainerFactory factory = new UndertowEmbeddedServletContainerFactory();
+		factory.addBuilderCustomizers(builder -> builder.setServerOption(ENABLE_HTTP2, true));
+		
+		return factory;
 	}
 }
