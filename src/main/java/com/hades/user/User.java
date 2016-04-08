1fceb8f20705948fe0c3eba283dbe1b00b11b4f6
Eric Dallo <ericdallo06@hotmail.com>
2016-04-08 19:23:20 -0300

    FIX-entity-user

--- src/main/java/com/hades/user/User.java
+++ src/main/java/com/hades/user/User.java
@@ -4,9 +4,11 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
-@Entity(name = "user")
+@Entity
+@Table(name = "user")
 public class User {
 	@Id
 	@GeneratedValue
