b5a17d5bfef9a880eb8c5661e9665cb1cf759074
Bruno Geronimo <brunogeronimo@brunogeronimo.com.br>
2016-05-17 13:06:56 -0300

    Tratamento aprimorado no SocialLogin
    Caso o usuario tenha se cadastrado mas nunca tenha se conectado por uma
    rede social, o login social sera criado.

--- src/main/java/com/hades/social/SocialLoginCallback.java
+++
@@ -1,9 +0,0 @@
-package com.hades.social;
-
-import org.springframework.social.connect.UserProfile;
-
-interface SocialLoginCallback {
-
-	String onSucess(UserProfile userProfile);
-	
-}
--- src/main/java/com/hades/social/SocialService.java
+++ src/main/java/com/hades/social/SocialService.java
@@ -30,41 +30,28 @@ public class SocialService {
 		String socialId = userProfile.getEmail();
 		System.out.println(socialId);
 		if (socialId == null){
-			return "hades.facebook.accessDenied";
+			return "hades.social.accessDenied";
 		}
 		Optional<SocialLogin> optionalSocialLogin = socialRepository.findBySocialId(socialId);
-		if (optionalSocialLogin.isPresent()){
+		if (optionalSocialLogin.isPresent()){ //user has been found
 			socialLogin = optionalSocialLogin.get();
 			tokenService.createTokenFor(socialLogin);
 			return socialLogin.getToken();
 		}else{
-			User user = new User(userProfile.getName(), userProfile.getEmail());
-			userRepository.save(user);
+			Optional<User> optionalUser = userRepository.findByEmail(userProfile.getEmail());
+			User user;
+			if (optionalUser.isPresent()){
+				//User exists, but don't have a social login
+				user = optionalUser.get();
+			}else{
+				//User do not exists, so create him	
+				user = new User(userProfile.getName(), userProfile.getEmail());
+				userRepository.save(user);
+			}
 			socialLogin = new SocialLogin(socialId, socialType, user);
 			socialRepository.save(socialLogin);
 			tokenService.createTokenFor(socialLogin);
 			return socialLogin.getToken();
 		}
-		
-		//Validar se o usuário já existe na tabela de social_info
-		//Cadastrar o usuário
-		
-		
-		
-//		LoginInfo loginInfo = new LoginInfo(email, defaultPassword, USER);
-//		loginInfo.setPassword(defaultPassword);
-//		try{
-//			LoginInfoDTO loginInfoDTO = loginService.login(loginInfo);
-//			return loginInfoDTO.getToken();
-//		}catch(Exception e){
-//			String name = userProfile.getName();
-//			UserDTO userDTO = new UserDTO(name, email, defaultPassword);
-//			User user = new User(userDTO.getName(), userDTO.getEmail());
-//			loginInfo = new LoginInfo(email, defaultPassword, LoginOrigin.USER);
-//			user.setLoginInfo(loginInfo);
-//			userRepository.save(user);
-//			tokenService.createTokenFor(loginInfo);
-//			return loginInfo.getToken();
-//		}
 	}
 }
