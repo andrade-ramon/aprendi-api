23b6c35de8c12bed9fc4da63cb323c43387af22f
Eric Dallo <ericdallo06@hotmail.com>
2016-05-21 22:38:59 -0300

    Corrige script nginx e startup

--- configuration/startup.sh
+++ configuration/startup.sh
@@ -5,15 +5,13 @@ set -v
 curl -s "https://storage.googleapis.com/signals-agents/logging/google-fluentd-install.sh" | bash
 service google-fluentd restart &
 
-export DEBIAN_FRONTEND=noninteractive
-
-export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+export GCSFUSE_REPO=gcsfuse-wily
 echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 # Install dependencies
 apt-get update
-apt-get install -yq openjdk-8-jdk nginx mysql-client gcsfuse
+apt-get install -yq openjdk-8-jdk nginx gcsfuse
 
 mkdir /opt/app
 git clone https://github.com/andrade-ramon/hades.git /opt/app
@@ -28,14 +26,12 @@ cp /mnt/bucket/hades/hades.key /opt/app/
 cp /mnt/bucket/hades/production.properties /opt/app/src/main/resources/application.properties
 
 # Nginx conf
-rm -f /etc/nginx/sites-enabled/default
-ln -s /opt/app/configuration/nginx.conf /etc/nginx/sites-enabled/
+ln -sf /opt/app/configuration/nginx.conf /etc/nginx/nginx.conf
 
 # Gradle release
 cd /opt/app
 ./gradlew build
 
-sudo nginx -s reload
-
 java -jar build/libs/hades.jar &
 
+systemctl restart nginx
