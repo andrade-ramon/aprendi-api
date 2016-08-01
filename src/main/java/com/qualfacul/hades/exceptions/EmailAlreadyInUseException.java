0235c0a24dbfc718def51af78e871d4a9fac5a63
Bruno Geronimo <brunogeronimo@brunogeronimo.com.br>
2016-08-01 01:12:25 -0300

    Comentario desnecessario removido

--- src/main/java/com/qualfacul/hades/exceptions/EmailAlreadyInUseException.java
+++ src/main/java/com/qualfacul/hades/exceptions/EmailAlreadyInUseException.java
@@ -1,6 +1,5 @@
 package com.qualfacul.hades.exceptions;
 
-//@ResponseStatus(value = HttpStatus.OK, reason = "This e-mail is already in use")
 public class EmailAlreadyInUseException extends Exception {
 	private static final long serialVersionUID = 1L;
 	public EmailAlreadyInUseException(){
