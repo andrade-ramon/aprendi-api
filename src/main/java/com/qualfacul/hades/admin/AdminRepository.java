7b87112c0b524b81cc783edb3ac8c73aec242c4b
Bruno Geronimo <contato@brunogeronimo.com.br>
2016-10-20 00:53:51 -0200

    Endpoints para criação e exclusão de administradores criado

--- src/main/java/com/qualfacul/hades/admin/AdminRepository.java
+++ src/main/java/com/qualfacul/hades/admin/AdminRepository.java
@@ -1,10 +1,16 @@
 package com.qualfacul.hades.admin;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.RepositoryDefinition;
 
 @RepositoryDefinition(domainClass = Admin.class, idClass = Long.class)
 public interface AdminRepository {
+	
+	Optional<Admin> findById(long id);
 
 	Admin save(Admin admin);
+	
+	void delete(Admin admin);
 
 }
---
+++ src/main/java/com/qualfacul/hades/exceptions/AdminNotFoundException.java
@@ -0,0 +1,10 @@
+package com.qualfacul.hades.exceptions;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = NOT_FOUND, reason = "Admin not found")
+public class AdminNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+}
