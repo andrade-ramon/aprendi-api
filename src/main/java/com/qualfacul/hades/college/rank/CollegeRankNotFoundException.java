7eb5fde93d801b31adc6d61e3bf70dab0a0d60d6
Eric Dallo <ericdallo06@hotmail.com>
2016-11-20 13:00:14 -0200

    Adiciona endpoint para retornar um ranking de uma faculdade

---
+++ src/main/java/com/qualfacul/hades/college/rank/CollegeRankNotFoundException.java
@@ -0,0 +1,10 @@
+package com.qualfacul.hades.college.rank;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = NOT_FOUND, reason = "CollegeRank not found")
+public class CollegeRankNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = -2552400284805320353L;
+}
